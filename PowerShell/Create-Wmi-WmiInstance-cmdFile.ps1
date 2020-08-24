9b186b788971e8fb79f91047077efa5271c43f16
AxelPotato <lelenyx@gmail.com>
2020-08-24 17:31:57 +0300

    bat file

---
+++ PowerShell/Create-Wmi-WmiInstance-cmdFile.ps1
@@ -0,0 +1,35 @@
+# Based on https://learn-powershell.net/2013/08/14/powershell-and-events-permanent-wmi-event-subscriptions/
+
+# Set the namespace
+$wmiParams = @{
+  NameSpace = 'root\subscription'
+}
+
+# Creating a new event filter
+$wmiParams.Class = '__EventFilter'
+$wmiParams.Arguments = @{
+  Name           = 'BugSecFilter'
+  EventNamespace = 'root\CIMV2'
+  QueryLanguage  = 'WQL'
+  Query          = "select * from __InstanceCreationEvent  within 5 where targetInstance isa 'Win32_Process' and TargetInstance.Name = 'chrome.exe'"
+}
+
+$filterResult = Set-WmiInstance @wmiParams
+
+# Creating a new consumer
+$wmiParams.Class = 'CommandLineEventConsumer'
+$wmiParams.Arguments = @{
+  Name                = 'BugSecConsumer'
+  CommandLineTemplate = "cmd.exe /c c:\test.bat"
+}
+
+$consumerResult = Set-WmiInstance @wmiParams
+
+# Bind filter to consumer
+$wmiParams.Class = '__FilterToConsumerBinding'
+$wmiParams.Arguments = @{
+  Filter   = $filterResult
+  Consumer = $consumerResult
+}
+
+Set-WmiInstance @wmiParams
